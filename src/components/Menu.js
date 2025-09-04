0fa0e8a42751897afac5a955fe836e303db1ee96
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-09-04 21:39:10 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,14 +8,18 @@ function Menu({ dishes, activeCategory }) {
 
   return (
     <div className="menu">
-      {filteredDishes.map((dish) => (
-        <div key={dish.id} className="menu-item">
-          <img src={dish.image} alt={dish.name} className="menu-img" />
-          <h3 className="menu-name">{dish.name}</h3>
-          <p className="menu-category">{dish.category}</p>
-          <p className="menu-price">{dish.price}</p>
-        </div>
-      ))}
+      {filteredDishes.length > 0 ? (
+        filteredDishes.map((dish) => (
+          <div key={dish.id} className="menu-item">
+            <img src={dish.image} alt={dish.name} className="menu-img" />
+            <h3 className="menu-name">{dish.name}</h3>
+            <p className="menu-category">{dish.category}</p>
+            <p className="menu-price">{dish.price}</p>
+          </div>
+        ))
+      ) : (
+        <p className="no-items">No items found</p>
+      )}
     </div>
   );
 }
